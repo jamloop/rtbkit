5a56925646f548e48d124e16f2bdcbb8372c4828
Mathieu Stefani <mathieu@datacratic.com>
2016-04-07 13:16:31 -0400

    Manually applied common.mk modification

--- rtbkit/rtbkit/common/common.mk
+++ rtbkit/rtbkit/common/common.mk
@@ -23,7 +23,8 @@ LIBRTB_SOURCES := \
 	win_cost_model.cc \
 	post_auction_proxy.cc \
 	analytics_publisher.cc \
-	bid_request_pipeline.cc
+	bid_request_pipeline.cc \
+	augmentor_interface.cc
 
 LIBRTB_LINK := \
 	ACE arch utils jsoncpp boost_thread endpoint boost_regex zmq opstats bid_request
