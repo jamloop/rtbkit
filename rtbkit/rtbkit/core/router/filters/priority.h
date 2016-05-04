2829a0859f6e948005be9764e9b38e73534f161b
jamloop <ops@jamloop.com>
2016-05-04 10:39:46 -0400

    Update priority.h

--- rtbkit/rtbkit/core/router/filters/priority.h
+++ rtbkit/rtbkit/core/router/filters/priority.h
@@ -23,7 +23,7 @@ struct Priority
         static constexpr unsigned WhiteBlackList   = 0x0300;
         static constexpr unsigned DeviceType       = 0x3600;
         static constexpr unsigned DMA              = 0x1400;
-        static constexpr unsigned Region          = 0x1400;
+        static constexpr unsigned Region           = 0x1400;
     };
 
     static constexpr unsigned Location             = 0x1000;
